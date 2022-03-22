eb6834c17ddb810cb17bc0d0fe111a0b66cd4e3d
jimatas <atask@hr.nl>
2022-03-22 11:15:31 +0100

    Add NullableExtensions and ExceptionExtensions. Version bump.

---
+++ HR.Common.Utilities/ExceptionExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace HR.Common.Utilities
+{
+    public static class ExceptionExtensions
+    {
+        /// <summary>
+        /// Returns a detailed error message for the exception.
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <param name="includeInnerExceptions">Indicates whether to include the details of any inner exceptions.</param>
+        /// <returns>A detailed description of the exception, including stack trace information.</returns>
+        public static string DetailMessage(this Exception exception, bool includeInnerExceptions = true)
+        {
+            var detailMessageBuilder = new StringBuilder(exception.BuildDetailMessage());
+
+            if (includeInnerExceptions)
+            {
+                exception.AppendInnerExceptionsTo(detailMessageBuilder);
+            }
+
+            return detailMessageBuilder.ToString();
+        }
+
+        private static string BuildDetailMessage(this Exception exception)
+        {
+            var detailMessage = $"{exception.GetType().FullName}: {exception.Message}";
+
+            if (!string.IsNullOrEmpty(exception.StackTrace))
+            {
+                detailMessage += Environment.NewLine + exception.StackTrace;
+            }
+
+            return detailMessage;
+        }
+
+        private static void AppendInnerExceptionsTo(this Exception exception, StringBuilder detailMessageBuilder, int depth = 0)
+        {
+            if (exception.InnerException != null)
+            {
+                depth++;
+                detailMessageBuilder.Append($" [{nameof(exception.InnerException)} ({depth}): {exception.InnerException.BuildDetailMessage()}]");
+
+                exception.InnerException.AppendInnerExceptionsTo(detailMessageBuilder, depth);
+            }
+        }
+    }
+}
---
+++ HR.Common.Utilities/NullableExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HR.Common.Utilities
+{
+    public static class NullableExtensions
+    {
+        /// <summary>
+        /// Determines whether the <see cref="Nullable{T}"/> is either <c>null</c> or has the default value for its underlying type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsNullOrDefault<T>(this T? value) where T : struct
+               => value.GetValueOrDefault().Equals(default(T));
+
+        /// <summary>
+        /// Provides functionality similar to C# 6.0's null conditional operator.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="target">The target object.</param>
+        /// <param name="expression">The expression to evaluate if the object is not <c>null</c>.</param>
+        /// <returns>If the object is not <c>null</c>, the value returned by the expression; otherwise, the default value for the expression's return type.</returns>
+        public static TResult IfNotNull<T, TResult>(this T target, Func<T, TResult> expression)
+            => target != null ? expression(target) : default;
+
+        /// <summary>
+        /// Provides functionality similar to C# 6.0's null conditional operator.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="target">The target object.</param>
+        /// <param name="expression">The expression to evaluate if the object is not <c>null</c>.</param>
+        public static void IfNotNull<T>(this T target, Action<T> expression)
+        {
+            if (target != null)
+            {
+                expression(target);
+            }
+        }
+    }
+}
