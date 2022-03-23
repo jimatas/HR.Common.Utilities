806b35becb747f04d268a9f4248bc300f684eaf7
jimatas <atask@hr.nl>
2022-03-23 19:52:04 +0100

    More unit tests added to project.

---
+++ HR.Common.Utilities.Tests/ReaderWriterLockSlimExtensionsTests.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Threading;
+
+namespace HR.Common.Utilities.Tests
+{
+    [TestClass]
+    public class ReaderWriterLockSlimExtensionsTests
+    {
+        [TestMethod]
+        public void EnterReadLockAndExit_ByDefault_ReturnsDisposable()
+        {
+            // Arrange
+            ReaderWriterLockSlim readWriteLock = new();
+
+            // Act
+            IDisposable disposable = readWriteLock.EnterReadLockAndExit();
+
+            // Assert
+            Assert.IsNotNull(disposable);
+            Assert.IsInstanceOfType(disposable, typeof(IDisposable));
+        }
+
+        [TestMethod]
+        public void EnterReadLockAndExit_Disposing_ExitsLock()
+        {
+            // Arrange
+            ReaderWriterLockSlim readWriteLock = new();
+            var countBeforeEnter = readWriteLock.CurrentReadCount;
+
+            // Act
+            IDisposable disposable = readWriteLock.EnterReadLockAndExit();
+            var countBeforeExit = readWriteLock.CurrentReadCount;
+            disposable.Dispose();
+            var countAfterExit = readWriteLock.CurrentReadCount;
+
+            // Assert
+            Assert.AreEqual(0, countBeforeEnter);
+            Assert.AreEqual(1, countBeforeExit);
+            Assert.AreEqual(0, countAfterExit);
+        }
+
+        [TestMethod]
+        public void EnterWriteLockAndExit_ByDefault_ReturnsDisposable()
+        {
+            // Arrange
+            ReaderWriterLockSlim readWriteLock = new();
+
+            // Act
+            IDisposable disposable = readWriteLock.EnterWriteLockAndExit();
+
+            // Assert
+            Assert.IsNotNull(disposable);
+            Assert.IsInstanceOfType(disposable, typeof(IDisposable));
+        }
+
+        [TestMethod]
+        public void EnterWriteLockAndExit_Disposing_ExitsLock()
+        {
+            // Arrange
+            ReaderWriterLockSlim readWriteLock = new();
+            var isLockedBeforeEnter = readWriteLock.IsWriteLockHeld;
+
+            // Act
+            IDisposable disposable = readWriteLock.EnterWriteLockAndExit();
+            var isLockedBeforeExit = readWriteLock.IsWriteLockHeld;
+            disposable.Dispose();
+            var isLockedAfterExit = readWriteLock.IsWriteLockHeld;
+
+            // Assert
+            Assert.IsFalse(isLockedBeforeEnter);
+            Assert.IsTrue(isLockedBeforeExit);
+            Assert.IsFalse(isLockedAfterExit);
+        }
+
+        [TestMethod]
+        public void EnterUpgradeableReadLockAndExit_ByDefault_ReturnsDisposable()
+        {
+            // Arrange
+            ReaderWriterLockSlim readWriteLock = new();
+
+            // Act
+            IDisposable disposable = readWriteLock.EnterUpgradeableReadLockAndExit();
+
+            // Assert
+            Assert.IsNotNull(disposable);
+            Assert.IsInstanceOfType(disposable, typeof(IDisposable));
+        }
+
+        [TestMethod]
+        public void EnterUpgradeableReadLockAndExit_Disposing_ExitsLock()
+        {
+            // Arrange
+            ReaderWriterLockSlim readWriteLock = new();
+            var isLockedBeforeEnter = readWriteLock.IsUpgradeableReadLockHeld;
+
+            // Act
+            IDisposable disposable = readWriteLock.EnterUpgradeableReadLockAndExit();
+            var isLockedBeforeExit = readWriteLock.IsUpgradeableReadLockHeld;
+            disposable.Dispose();
+            var isLockedAfterExit = readWriteLock.IsUpgradeableReadLockHeld;
+
+            // Assert
+            Assert.IsFalse(isLockedBeforeEnter);
+            Assert.IsTrue(isLockedBeforeExit);
+            Assert.IsFalse(isLockedAfterExit);
+        }
+    }
+}
---
+++ HR.Common.Utilities.Tests/SemaphoreSlimExtensionsTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HR.Common.Utilities.Tests
+{
+    [TestClass]
+    public class SemaphoreSlimExtensionsTests
+    {
+        [TestMethod]
+        public void WaitAndRelease_ByDefault_ReturnsDisposable()
+        {
+            // Arrange
+            SemaphoreSlim semaphore = new(initialCount: 1, maxCount: 1);
+
+            // Act
+            IDisposable disposable = semaphore.WaitAndRelease();
+
+            // Assert
+            Assert.IsNotNull(disposable);
+            Assert.IsInstanceOfType(disposable, typeof(IDisposable));
+        }
+
+        [TestMethod]
+        public void WaitAndRelease_Disposing_ReleasesSemaphore()
+        {
+            // Arrange
+            SemaphoreSlim semaphore = new(initialCount: 1, maxCount: 1);
+            var countBeforeWait = semaphore.CurrentCount;
+
+            // Act
+            IDisposable disposable = semaphore.WaitAndRelease();
+            var countBeforeRelease = semaphore.CurrentCount;
+            disposable.Dispose();
+            var countAfterRelease = semaphore.CurrentCount;
+
+            // Assert
+            Assert.AreEqual(1, countBeforeWait);
+            Assert.AreEqual(0, countBeforeRelease);
+            Assert.AreEqual(1, countAfterRelease);
+        }
+
+        [TestMethod]
+        public async Task WaitAndReleaseAsync_ByDefault_ReturnsDisposable()
+        {
+            // Arrange
+            SemaphoreSlim semaphore = new(initialCount: 1, maxCount: 1);
+
+            // Act
+            IDisposable disposable = await semaphore.WaitAndReleaseAsync();
+
+            // Assert
+            Assert.IsNotNull(disposable);
+            Assert.IsInstanceOfType(disposable, typeof(IDisposable));
+        }
+
+        [TestMethod]
+        public async Task WaitAndReleaseAsync_Disposing_ReleasesSemaphore()
+        {
+            // Arrange
+            SemaphoreSlim semaphore = new(initialCount: 1, maxCount: 1);
+            var countBeforeWait = semaphore.CurrentCount;
+
+            // Act
+            IDisposable disposable = await semaphore.WaitAndReleaseAsync();
+            var countBeforeRelease = semaphore.CurrentCount;
+            disposable.Dispose();
+            var countAfterRelease = semaphore.CurrentCount;
+
+            // Assert
+            Assert.AreEqual(1, countBeforeWait);
+            Assert.AreEqual(0, countBeforeRelease);
+            Assert.AreEqual(1, countAfterRelease);
+        }
+    }
+}
