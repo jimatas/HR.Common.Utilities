496582267a93a1b66489001c99a5ede547562b78
jimatas <atask@hr.nl>
2022-06-12 00:02:53 +0200

    Removed IfNotNull extension methods.

--- HR.Common.Utilities.Tests/NullableExtensionsTests.cs
+++ HR.Common.Utilities.Tests/NullableExtensionsTests.cs
@@ -7,7 +7,6 @@ namespace HR.Common.Utilities.Tests
     [TestClass]
     public class NullableExtensionsTests
     {
-        #region IsNullOrDefault
         [DataTestMethod]
         [DataRow(null)]
         [DataRow(default(int))]
@@ -62,76 +61,5 @@ namespace HR.Common.Utilities.Tests
             // Assert
             Assert.IsFalse(result);
         }
-        #endregion
-
-        #region IfNotNull
-        [TestMethod]
-        public void IfNotNull_CalledOnNullGivenExpressionReturningNewObject_ReturnsNull()
-        {
-            // Arrange
-            object? target = null;
-
-            // Act
-            object? result = target.IfNotNull(_ => new object());
-
-            // Assert
-            Assert.IsNull(result);
-        }
-
-        [TestMethod]
-        public void IfNotNull_CalledOnNonNullGivenExpressionReturningNewObject_ReturnsNewObject()
-        {
-            // Arrange
-            object? target = new();
-
-            // Act
-            object? result = target.IfNotNull(_ => new object());
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.AreNotEqual(result, target);
-        }
-
-        [TestMethod]
-        public void IfNotNull_CalledOnNonNullGivenExpressionReturningSelf_ReturnsSelf()
-        {
-            // Arrange
-            object? target = new();
-
-            // Act
-            object? result = target.IfNotNull(self => self);
-
-            // Assert
-            Assert.AreEqual(result, target);
-        }
-
-        [TestMethod]
-        public void IfNotNull_CalledOnNullGivenAction_DoesNotInvokeAction()
-        {
-            // Arrange
-            object? target = null;
-            bool result = false;
-
-            // Act
-            target.IfNotNull(_ => { result = true; });
-
-            // Assert
-            Assert.IsFalse(result);
-        }
-
-        [TestMethod]
-        public void IfNotNull_CalledOnNonNullGivenAction_InvokesAction()
-        {
-            // Arrange
-            object? target = new();
-            bool result = false;
-
-            // Act
-            target.IfNotNull(_ => { result = true; });
-
-            // Assert
-            Assert.IsTrue(result);
-        }
-        #endregion
     }
 }
